3240edb438fe44f0afef81ee50cfb200364dac12
James Requenez <jrequenez@hotmail.com>
2023-09-05 12:42:10 -0500

    🎉  chore: create console and test projects

---
+++ GameConsole/GameConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GameConsole;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting Project 'Akka.NET Peristence Fundamentals!");
+    }
+}
+
